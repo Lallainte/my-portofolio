e37d316d949174a20ebbaa758e71865f96102d50
Lallainte <lallainte11@gmail.com>
2024-08-05 02:10:25 +0700

    4

--- project1/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  // hilangkan tombol cari
-  $("#tombol-cari").hide();
-
-  // event ketika keyword di tulis
-  $("#keyword").on("keyup", function () {
-    $("#container").load("ajax/pegawai.php?keyword=" + $("#keyword").val());
-  });
-});
